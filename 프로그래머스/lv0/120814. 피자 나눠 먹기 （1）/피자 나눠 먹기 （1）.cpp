f5f036bcb5c81f524832506e666613196a049921
SeonGuk Kim <tjsrnr3180@gmail.com>
2023-04-04 03:44:13 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    
+    answer = n / 7;
+    if (n % 7)
+    {
+        answer += 1;
+    }
+    
+    return answer;
+}
