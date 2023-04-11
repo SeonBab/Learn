6035229f6df820c8180c23092563e2fa872b2179
SeonGuk Kim <tjsrnr3180@gmail.com>
2023-04-12 03:15:46 +0900

    [level 0] Title: 가위 바위 보, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string rsp) {
+    string answer = "";
+    for (auto e : rsp)
+    {
+        switch (e)
+        {
+            case '0':
+                answer += '5';
+                break;
+            case '2':
+                answer += '0';
+                break;
+            case '5':
+                answer += '2';
+                break;
+        }
+    }
+    return answer;
+}
