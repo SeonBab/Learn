ff5d6301b266c0a3aea692899d2c5d834ceb734a
SeonGuk Kim <tjsrnr3180@gmail.com>
2023-04-14 03:57:26 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    
+    while (order)
+    {
+        if (order % 10 % 3 == 0 && order % 10 != 0)
+        {
+            ++answer;
+        }
+        
+        order /= 10;
+    }
+    return answer;
+}
