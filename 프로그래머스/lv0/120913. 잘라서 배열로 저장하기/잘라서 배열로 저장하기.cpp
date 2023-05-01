74186960b273867c773ed81b53571ad9b47ea524
SeonGuk Kim <tjsrnr3180@gmail.com>
2023-05-01 22:49:50 +0900

    [level 0] Title: 잘라서 배열로 저장하기, Time: 0.07 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120913. 잘라서 배열로 저장하기/잘라서 배열로 저장하기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> solution(string my_str, int n) {
+    vector<string> answer;
+
+    for (int i = 0; i < my_str.length(); i += n)
+    {
+        answer.push_back(my_str.substr(i, n));
+    }
+
+    return answer;
+}
