231515e58768e4a35eb622af3af4f527a0624d0c
SeonGuk Kim <tjsrnr3180@gmail.com>
2023-05-10 23:56:27 +0900

    [level 0] Title: A로 B 만들기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120886. A로 B 만들기/A로 B 만들기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string before, string after) {
+    int answer = 0;
+
+    sort(before.begin(), before.end());
+    sort(after.begin(), after.end());
+    
+    if (before == after)
+    {
+        ++answer;
+    }
+
+    return answer;
+}
