d1c8367e863804b6ddbfc1792546d4ae5cfb58de
SeonGuk Kim <tjsrnr3180@gmail.com>
2023-06-18 13:24:58 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	int T, a, b;
+
+	cin >> T;
+
+	for (int t = 0; t < T; t++)
+	{
+		cin >> a >> b;
+		cout << a + b << endl;
+	}
+
+}
