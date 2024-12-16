9cb654348aaac7c6f2114e2e4a65cd17fb59ec07
SeonGuk Kim <tjsrnr3180@gmail.com>
2024-12-16 23:43:24 +0900

    [level 0] Title: 짝수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.cpp
@@ -1,5 +1,10 @@
 int solution(int n) {
-    int answer = (n / 2);
-    answer *= answer + 1;
+    int answer = 0;
+
+    for (int i = 2; i <= n; i += 2)
+    {
+        answer += i;
+    }
+
     return answer;
 }
