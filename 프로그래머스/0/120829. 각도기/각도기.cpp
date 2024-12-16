cd06c8895c119aca056cba76bcd177e49d56ea2e
SeonGuk Kim <tjsrnr3180@gmail.com>
2024-12-16 21:13:24 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 3.59 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.cpp
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -1,10 +1,5 @@
 int solution(int angle) {
-    int answer = 0;
-    
-    if (0 < angle && angle < 90) answer = 1;
-    else if (angle == 90) answer = 2;
-    else if (90 < angle && angle < 180) answer = 3;
-    else if (angle == 180) answer = 4;
+    int answer = (angle % 90 == 0) ? angle / 90 * 2 : angle / 90 * 2 + 1;
     
     return answer;
 }
