efeee6de98993a9a094ca16144a75fe3f2f3e324
SeonGuk Kim <tjsrnr3180@gmail.com>
2024-12-18 01:20:56 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 4.02 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
@@ -1,20 +1,24 @@
 #include <vector>
 
+int GetGCD(int a, int b) {
+    if (a % b == 0)
+    {
+        return b;
+    }
+
+    return GetGCD(b, a % b);
+}
+
 std::vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
     std::vector<int> answer;
 
     int numerator = numer1 * denom2 + numer2 * denom1;
     int denominator = denom1 * denom2;
 
-    for (int i = std::min(denominator, numerator); i >= 2; i--)
-    {
-        if (denominator % i == 0 && numerator % i == 0)
-        {
-            denominator /= i;
-            numerator /= i;
-            break;
-        }
-    }
+    int gcdResult = GetGCD(numerator, denominator);
+
+    numerator /= gcdResult;
+    denominator /= gcdResult;
 
     answer.push_back(numerator);
     answer.push_back(denominator);
