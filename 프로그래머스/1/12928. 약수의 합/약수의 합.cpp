5be496d49398755e4306769947d7cbbb035dc4f9
SeonGuk Kim <tjsrnr3180@gmail.com>
2024-12-21 00:54:54 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.18 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cpp
@@ -1,12 +1,20 @@
+#include <cmath>
+
 int solution(int n) {
     int answer = 0;
-    
-    for(int i = 1; i <= n; ++i)
+
+    for (int i = 1; i <= std::sqrt(n); ++i)
     {
         if (n % i == 0)
         {
             answer += i;
+
+            if (i != n / i)
+            {
+                answer += n / i;
+            }
         }
     }
+
     return answer;
 }
