013a02b27f2cfd4406f8cd908b95e680f63f687b
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-02 11:23:39 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 5.05 ms, Memory: 6.07 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, int divisor) {
+    vector<int> answer;
+    
+    sort(arr.begin(), arr.end());
+    
+    for (int i = 0; i < arr.size(); ++i)
+    {
+        if (arr[i] % divisor == 0)
+        {
+            answer.push_back(arr[i]);
+        }
+    }
+    
+    if (answer.size() == 0)
+    {
+        answer.push_back(-1);
+    }
+    
+    return answer;
+}
