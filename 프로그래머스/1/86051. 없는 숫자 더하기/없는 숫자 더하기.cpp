2973462e53c1ad57a85e234d8b8c4b7924e756c3
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-02 20:26:51 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -3,22 +3,12 @@
 using namespace std;
 
 int solution(vector<int> numbers) {
-    int answer = 0;
-    
-    vector<bool> numList (10);
-    
+    int answer = 45;
+
     for (int i = 0; i < numbers.size(); ++i)
     {
-        numList[numbers[i]] = true;
+        answer -= numbers[i];
     }
-    
-    for (int i = 0; i < numList.size(); ++i)
-    {
-        if (numList[i] == false)
-        {
-            answer += i;
-        }
-    }
-    
+
     return answer;
 }
