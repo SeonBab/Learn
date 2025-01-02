19815d1dbb8d0d0632891e2d87a53df0f8843cf1
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-02 11:48:25 +0900

    [level 1] Title: 음양 더하기, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> absolutes, vector<bool> signs) {
+    int answer = 0;
+
+    for (int i = 0; i < signs.size() ; ++i)
+    {
+        if(false == signs[i])
+        {
+            absolutes[i] *= -1;
+        }
+        
+        answer += absolutes[i];
+    }
+
+    return answer;
+}
