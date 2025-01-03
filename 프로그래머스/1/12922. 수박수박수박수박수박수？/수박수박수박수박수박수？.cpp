cb8bd9bc782c53ca5e81a9cadb9b61490539a8a7
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-03 22:14:29 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.11 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cpp
@@ -0,0 +1,19 @@
+#include <string>
+
+std::string solution(int n) {
+    std::string answer = "";
+    
+    for (int i = 0; i < n; ++i)
+    {
+        if(i % 2 == 0)
+        {
+            answer += "수";
+        }
+        else
+        {
+            answer += "박";
+        }
+    }
+    
+    return answer;
+}
