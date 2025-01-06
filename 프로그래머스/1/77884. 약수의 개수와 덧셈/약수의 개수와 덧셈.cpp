14606e2b28a1f1f3febfa7f695947b7d25298af8
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-06 09:55:17 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.01 ms, Memory: 4.23 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -1,34 +1,19 @@
+#include <cmath>
+
 int solution(int left, int right) {
     int answer = 0;
     
     for (int i = left; i <= right; ++i)
     {
-        int count = 0;
+        int sqrtValue = sqrt(i);
         
-        for (int j = 1; j * j <= i; ++j)
+        if (sqrtValue * sqrtValue == i)
         {
-            if (i % j != 0)
-            {
-                continue;
-            }
-            
-            if (i / j == j)
-            {
-                count += 1;
-            }
-            else
-            {
-                count += 2;
-            }
-        }
-        
-        if (count % 2 == 0)
-        {
-            answer += i;
+            answer -= i;
         }
         else
         {
-            answer -= i;
+            answer += i;
         }
     }
     
