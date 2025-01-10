7edc3e646864b397f1c11df24a4351dc9fab7db6
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-10 10:04:34 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.cpp
@@ -3,18 +3,18 @@
 int solution(int n) {
     int answer = 0;
 
-    std::vector<int> str;
+    std::vector<int> vec;
     while (n)
     {
-        str.push_back(n % 3);
+        vec.push_back(n % 3);
         n /= 3;
     }
 
-    int three = 1;
-    for (int i = str.size() - 1; i >= 0; --i)
+    int power = 1;
+    for (int i = vec.size() - 1; i >= 0; --i)
     {
-        answer += str[i] * three;
-        three *= 3;
+        answer += vec[i] * power;
+        power *= 3;
     }
 
     return answer;
