f544894b0f950a73568721170a3ba6c51d4b4a6f
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-16 02:58:15 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.18 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cpp
@@ -2,29 +2,16 @@
 
 int solution(std::string t, std::string p) {
     int answer = 0;
-    long long pNum = 0;
-    
-    for (int i = 0; i < p.length(); ++i)
-    {
-        pNum *= 10;
-        pNum += static_cast<int>(p[i]) - '0';
-    }
     
     for (int i = 0; i <= t.length() - p.length(); ++i)
     {
-        long long tNum = 0;
+        std::string str = t.substr(i, p.length());
         
-        for (int j = 0; j < p.length(); ++j)
-        {
-            tNum *= 10;
-            tNum += static_cast<int>(t[i + j]) - '0';
-        }
-                
-        if (tNum <= pNum)
+        if (str <= p)
         {
             ++answer;
         }
     }
     
-    return answer;
+    return answer;;
 }
