2d134d639b167f3d3d91018c502a308c81997188
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-20 09:25:05 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <algorithm>
+
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+
+    for (int i = 0; i < commands.size(); ++i)
+    {
+        vector<int> vec{ array };
+        
+        int start = commands[i][0] - 1;
+        int end = commands[i][1];
+        int numIndex = start + commands[i][2] - 1;
+
+        sort(vec.begin() + start, vec.begin() + end);
+
+        answer.push_back(vec[numIndex]);
+    }
+    
+    
+    return answer;
+}
