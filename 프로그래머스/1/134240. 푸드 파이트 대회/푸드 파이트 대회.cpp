b46183b09677206a700f17fac104bde164406644
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-21 21:18:25 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.28 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 
 string solution(vector<int> food) {
-    string answer = "0";
+    string answer = "";
     
-    for (int i = food.size() - 1; i > 0 ; --i)
+    for (int i = 1; i < food.size(); ++i)
     {
         int loopCount = food[i] / 2;
         for (int j = 0; j < loopCount; ++j)
         {
-            answer = to_string(i) + answer + to_string(i);
+            answer += to_string(i);
         }
+    }
+    
+    answer += '0';
+    
+    for (int i = answer.length() - 2; i >= 0; --i)
+    {
+        answer += answer[i];
     }
     
     return answer;
