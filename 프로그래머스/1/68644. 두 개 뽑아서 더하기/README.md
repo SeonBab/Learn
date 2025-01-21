8d997959053754764e90283068e24b5d2b2ef84a
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-21 11:14:40 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.23 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 두 개 뽑아서 더하기 - 68644 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68644) 
 
 ### 성능 요약
 
-메모리: 4.2 MB, 시간: 0.56 ms
+메모리: 3.69 MB, 시간: 0.23 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 21일 11:01:21
+2025년 01월 21일 11:14:36
 
 ### 문제 설명
 
--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.cpp
@@ -1,34 +1,21 @@
 #include <vector>
-#include <algorithm>
+#include <set>
 
 using namespace std;
 
 vector<int> solution(vector<int> numbers) {
     vector<int> answer;
+    set<int> temp;
     
     for (int i = 0; i < numbers.size() - 1; ++i)
     {
         for (int j = i + 1; j < numbers.size(); ++j)
         {
-            int sumNumber = numbers[i] + numbers[j];
-            bool bIsFind = false;
-            
-            for (int k = 0; k < answer.size(); ++k)
-            {
-                if (answer[k] == sumNumber)
-                {
-                    bIsFind = true;
-                }
-            }
-            
-            if (false == bIsFind)
-            {
-                answer.push_back(sumNumber);
-            }
+            temp.insert(numbers[i] + numbers[j]);
         }
     }
     
-    sort(answer.begin(), answer.end());
+    answer.assign(temp.begin(), temp.end());
     
     return answer;
 }
