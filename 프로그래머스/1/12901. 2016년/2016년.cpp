2a0d4ab993505d37b5e4a43024b7d0deb9454228
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-24 02:02:22 +0900

    [level 1] Title: 2016년, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.cpp
+++ 프로그래머스/1/12901. 2016년/2016년.cpp
@@ -1,27 +1,14 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 
 std::string solution(int a, int b) {
     std::string answer = "";
     int totalDate = 0;
     
     std::vector<std::string> week{ "THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED" };
-    std::unordered_map<int, int> date;
-    date[1] = 31;
-    date[2] = 29;
-    date[3] = 31;
-    date[4] = 30;
-    date[5] = 31;
-    date[6] = 30;
-    date[7] = 31;
-    date[8] = 31;
-    date[9] = 30;
-    date[10] = 31;
-    date[11] = 30;
-    date[12] = 12;
+    std::vector<int> date{31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
     
-    for (int i = 1; i < a; ++i)
+    for (int i = 0; i < a - 1; ++i)
     {
         totalDate += date[i];
     }
