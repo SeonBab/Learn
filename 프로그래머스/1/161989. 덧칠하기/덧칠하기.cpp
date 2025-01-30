6f7a7c6d44fcce93764891c3c612b535b6bcecc9
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-30 18:43:36 +0900

    [level 1] Title: 덧칠하기, Time: 0.23 ms, Memory: 5.78 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <limits>
+
+#include <iostream>
+
+using namespace std;
+
+int solution(int n, int m, vector<int> section) {
+    int answer = 1;
+    
+    int rollerStart = section[0];
+    
+    for (int i = 1; i < section.size(); ++i)
+    {   
+        if (section[i] - rollerStart >= m)
+        {
+            rollerStart = section[i];
+            ++answer;
+        }
+    }
+    
+    return answer;
+}
