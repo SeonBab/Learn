ffd4b274b5ea591298d40db488ace487e8d4b9e0
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-02-02 03:28:25 +0900

    [level 1] Title: 옹알이 (2), Time: 0.58 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<string> babbling) {
+    int answer = 0;
+    
+    vector<string> babyWords { "aya", "ye", "woo", "ma" };
+    
+    for (int i = 0; i < babbling.size(); ++i)
+    {
+        string substr;
+        string found;
+        
+        for (int j = 0; j < babbling[i].size(); ++j)
+        {
+            substr += babbling[i][j];
+            
+            for (int k = 0; k < babyWords.size(); ++k)
+            {
+                
+                if (substr == babyWords[k] && substr != found)
+                {
+                    cout << substr << endl;
+                    found = substr;
+                    substr = "";
+                }
+            }
+        }
+        
+        if (substr == "")
+        {
+            ++answer;
+        }
+    }
+    
+    return answer;
+}
