bb4778018fdcb68e85726db0bab20e17c712a7d8
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-02-03 12:35:29 +0900

    [level 1] Title: 숫자 짝꿍, Time: 15.53 ms, Memory: 29 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -0,0 +1,47 @@
+#include <string>
+
+using namespace std;
+
+string solution(string X, string Y) {
+    string answer = "";
+    int xArray[10] { 0 };
+    int yArray[10] { 0 };
+    
+    for(int i = 0; i < X.length(); ++i)
+    {
+        int xInt = X[i] - '0';
+        ++xArray[xInt];
+    }
+        
+    for(int i = 0; i < Y.length(); ++i)
+    {
+        int yInt = Y[i] - '0';
+        ++yArray[yInt];
+    }
+    
+    for(int i = 9; i >= 0; --i)
+    {
+        if (xArray[i] == 0 || yArray[i] == 0)
+        {
+            continue;
+        }
+                
+        int minValue = (xArray[i] < yArray[i]) ? xArray[i] : yArray[i];
+        
+        for (int j = 0; j < minValue; ++j)
+        {
+            answer += '0' + i;
+        }
+    }
+    
+    if (answer.size() == 0)
+    {
+        answer = "-1";
+    }
+    else if (answer[0] == '0')
+    {
+        answer = '0';
+    }
+    
+    return answer;
+}
