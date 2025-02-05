bf3e13d8af532d2dbd7e4139bfb9e10f74c08def
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-02-05 11:20:29 +0900

    [level 1] Title: 문자열 나누기, Time: 0.05 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    
+    char key = 0;
+    int xCount = 0;
+    int otherCount = 0;
+    for (int i = 0; i < s.length(); ++i)
+    {
+        if (key == 0)
+        {
+            key = s[i];
+        }
+        
+        (key == s[i]) ? ++xCount : ++otherCount;
+        
+        if (xCount == otherCount)
+        {
+            ++answer;
+            key = 0;
+            xCount = 0;
+            otherCount = 0;
+        }
+    }
+    
+    if (key != 0)
+    {
+        ++answer;
+    }
+    
+    return answer;
+}
