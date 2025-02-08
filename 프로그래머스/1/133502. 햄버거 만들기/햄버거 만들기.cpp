00e1c8b291c13f1006ccaeac81e96bd6f03050d4
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-02-08 14:19:56 +0900

    [level 1] Title: 햄버거 만들기, Time: 13.11 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+
+    vector<int> hamburger;
+    
+    for(int e : ingredient)
+    {
+        hamburger.push_back(e);
+        
+        if (hamburger.size() >= 4 &&
+            hamburger[hamburger.size() - 4] == 1 &&
+            hamburger[hamburger.size() - 3] == 2 &&
+            hamburger[hamburger.size() - 2] == 3 &&
+            hamburger[hamburger.size() - 1] == 1)
+        {
+            hamburger.erase(hamburger.end() - 4, hamburger.end());
+            ++answer;
+        }
+    }
+    
+    return answer;
+}
