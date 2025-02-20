3e695b55b3b707c9a3c4db169c9292e18e08b01b
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-02-20 10:29:27 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.cpp
@@ -2,16 +2,17 @@ int solution(int n, int a, int b)
 {
     int answer = 0;
     
-    for (int i = 0; ; ++i)
+    while (true)
     {
         if (a == b)
         {
-            answer = i;
             break;
         }
         
         a = (a % 2 == 0) ? a / 2 : (a + 1) / 2;
         b = (b % 2 == 0) ? b / 2 : (b + 1) / 2;
+        
+        ++answer;
     }
 
     return answer;
