dfe07aac26ee5324d34e449ddf3a16e171f385d4
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-06-22 23:20:32 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> id_pw, vector<vector<string>> db) {
+    string answer = "fail";
+    
+    for (int i = 0; i < db.size(); ++i)
+    {
+        if (db[i][0] == id_pw[0])
+        {
+            if (db[i][1] == id_pw[1])
+            {
+                answer = "login";
+            }
+            else
+            {
+                answer = "wrong pw";
+            }
+        }
+    }
+    
+    return answer;
+}
