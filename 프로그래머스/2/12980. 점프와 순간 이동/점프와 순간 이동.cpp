85dc9df931db5bead538a97086241a3b7ec9ff21
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-10-06 02:22:32 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.75 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -0,0 +1,16 @@
+int solution(int n)
+{
+    int ans = 0;
+    
+    while (n)
+    {
+        if (n % 2 == 1)
+        {
+            ++ans;
+        }
+        
+        n /= 2;
+    }
+
+    return ans;
+}
