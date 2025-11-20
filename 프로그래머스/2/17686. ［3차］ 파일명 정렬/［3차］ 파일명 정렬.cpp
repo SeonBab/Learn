e308af4d39ec67ffd741e793c23be3755603cba7
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-11-20 21:03:39 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 2.59 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.cpp
@@ -0,0 +1,68 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+string lowerHead(string str)
+{
+    string lowerHead;
+    for (int i = 0; i < str.size(); ++i)
+    {
+        if (isdigit(str[i]))
+        {
+            break;
+        }
+        
+        lowerHead += tolower(str[i]);
+    }
+    
+    return lowerHead;
+}
+
+int getNumber(string str)
+{
+    auto firstDigitIt = find_if(str.begin(), str.end(), [](char c)
+                                { return isdigit(c); });
+    int firstDigitIndex = distance(str.begin(), firstDigitIt);
+    
+    auto endDigitIt = firstDigitIt;
+    while (endDigitIt != str.end() && isdigit(*endDigitIt))
+    {
+        ++endDigitIt;
+    }
+    
+    int length = distance(firstDigitIt, endDigitIt);
+    
+    string numStr = str.substr(firstDigitIndex, length);
+    
+    return stoi(numStr);
+}
+
+bool compare(string a, string b)
+{
+    string aHead = lowerHead(a);
+    string bHead = lowerHead(b);
+    
+    // HEAD 사전순 확인
+    if (aHead != bHead)
+    {
+        return aHead < bHead;
+    }
+    
+    int aNum = getNumber(a);
+    int bNum = getNumber(b);
+    
+    if (aNum != bNum)
+    {
+        return aNum < bNum;
+    }
+    
+    return false;
+}
+
+vector<string> solution(vector<string> files) {
+    stable_sort(files.begin(), files.end(), compare);
+    return files;
+}
