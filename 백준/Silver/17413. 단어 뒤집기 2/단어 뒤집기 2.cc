b217883763b38e9959eb4d7d3a3655abef365a58
SeonGuk Kim <tjsrnr3180@gmail.com>
2026-04-06 19:05:15 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 4 ms, Memory: 2508 KB -BaekjoonHub

--- 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.cc
@@ -1,100 +1,63 @@
 #include <iostream>
 #include <string>
-#include <vector>
-
-std::string ReserveWord(std::string str)
-{
-    std::string result;
-
-    int BackIndex = str.length();
-    if (str.back() == ' ')
-    {
-        BackIndex = str.length() - 1;
-    }
-
-    for (int i = BackIndex - 1; i >= 0; --i)
-    {
-        result += str[i];
-    }
-
-    if (str.back() == ' ')
-    {
-        result += str[BackIndex];
-    }
-    
-    return result;
-}
-
-std::vector<std::string> SubString(std::string str)
-{
-    std::vector<std::string> Result;
-
-    std::string SubStr;
-
-    bool IsSymbolStart = false;
-
-    for (int i = 0; i < str.size(); ++i)
-    {
-        // 특수문자 단어 시작
-        if (str[i] == '<')
-        {
-            IsSymbolStart = true;
-
-            // 문자열이 비어있지 않은 경우 단어의 끝
-            if (!SubStr.empty())
-            {
-                Result.push_back(SubStr);
-                SubStr.clear();
-            }
-        }
-        
-        SubStr += str[i];
-        
-        // 특수문자가 끝날 경우 단어의 끝
-        if (str[i] == '>')
-        {
-            IsSymbolStart = false;
-            Result.push_back(SubStr);
-            SubStr.clear();
-        }
-        // 특수문자 안에 있지 않은 공백일 경우 단어의 끝이다.
-        else if (IsSymbolStart == false && str[i] == ' ')
-        {
-            Result.push_back(SubStr);
-            SubStr.clear();
-        }
-    }
-
-    // 마지막 문자가 남아있을 경우
-    if (!SubStr.empty())
-    {
-        Result.push_back(SubStr);
-    }
-
-    return Result;
-}
 
 int main()
 {
-    std::string InputString;
-    getline(std::cin, InputString);
+    std::string InputStr;
+    getline(std::cin, InputStr);
 
-    std::vector<std::string> SubStringArr = SubString(InputString);
+    std::string Result;
+    std::string Temp;
 
-    std::string ResultStr;
-    for (int i = 0; i < SubStringArr.size(); ++i)
+    bool InTag = false;
+
+    for (char C : InputStr)
     {
-        std::string SubStr = SubStringArr[i];
-
-        if (SubStr.front() == '<')
+        if (C == '<')
         {
-            ResultStr += SubStr;
+            // 단어 끝이므로, 기존 단어 뒤집기
+            for (int i = Temp.size() - 1; i >= 0; --i)
+            {
+                Result += Temp[i];
+            }
+                
+            Temp.clear();
+
+            InTag = true;
+            Result += C;
+        }
+        else if (C == '>')
+        {
+            InTag = false;
+            Result += C;
+        }
+        else if (InTag)
+        {
+            Result += C;
         }
         else
         {
-            ResultStr += ReserveWord(SubStr);
+            if (C == ' ')
+            {
+                // 단어 끝이므로, 기존 단어 뒤집기
+                for (int i = Temp.size() - 1; i >= 0; --i)
+                    Result += Temp[i];
+                Temp.clear();
+
+                Result += ' ';
+            }
+            else
+            {
+                Temp += C;
+            }
         }
     }
 
-    std::cout << ResultStr << std::endl;
+    // 마지막 단어 처리
+    for (int i = Temp.size() - 1; i >= 0; --i)
+    {
+        Result += Temp[i];
+    }
+
+    std::cout << Result << std::endl;
 }
